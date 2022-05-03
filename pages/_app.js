6451634c150a2a722319b81c9efeffc2a562152a
Sagar Gugwad <saagar.gugwad@gmail.com>
2022-05-03 16:23:59 -0400

    testing SEO

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>My new cool app</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
